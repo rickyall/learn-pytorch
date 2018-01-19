8bd3084b5bb827dcf0655404399cd0f3f35dd4f3
qisc <qisc@thfund.com.cn>
2018-01-19 23:46:43 +0800

    重新调整目录，方便拓展

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # README #
 - 完成pytorch的官方教程， 掌握API方法
+- 添加gym强化学习项目
+
+# 文档
+## official
+- T1-Deep Learning with PyTorch  A 60 Minute Blitz
+- T2-Reinforcement Learning (DQN) tutorial
+- T3-Training a classifier
+- T4-Classifying Names with a Character-Level RNN
+- T5-Translation with a Sequence to Sequence Network and Attention
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mytorch',
+    version='0.0.0',
+    description='exponential enhancement strategy',
+    packages=find_packages(exclude=[]),
+    author='qisc',
+    author_email='783814127@qq.com',
+    package_data={'': ['*.*']},
+    url='https://github.com/rickyall/learn-pytorch',
+)
